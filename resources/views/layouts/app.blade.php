bcfec138190c5580045135727d3a4c0676d9ef0a
Hazem7575 <100474135+Hazem7575@users.noreply.github.com>
2023-02-22 18:37:08 +0200

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,6 @@
+@php
+    $assets = 1;
+@endphp
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl">
 <head>
@@ -5,18 +8,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-    <link rel="icon" href="{{asset('assets/img/favicon.png')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/fontawesome.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/slick.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/lightgallery.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/jQueryUi.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/textRotate.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/select2.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/droidarabickufi.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/rtl.css')}}" />
+    <link rel="icon" href="{{asset('assets/img/favicon.png')}}?v={{$assets}}?v={{$assets}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}?v={{$assets}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/fontawesome.css')}}?v={{$assets}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/slick.css')}}?v={{$assets}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/lightgallery.min.css')}}?v={{$assets}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}?v={{$assets}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/jQueryUi.min.css')}}?v={{$assets}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/textRotate.css')}}?v={{$assets}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/select2.min.css')}}?v={{$assets}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}?v={{$assets}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/droidarabickufi.css')}}?v={{$assets}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/rtl.css')}}?v={{$assets}}" />
     @yield('style')
 </head>
 <body class="font-sans antialiased">
@@ -133,19 +136,19 @@
     </div>
 </footer>
 
-<script src="{{asset('assets/js/vendor/modernizr-3.5.0.min.js')}}"></script>
-<script src="{{asset('assets/js/vendor/jquery-1.12.4.min.js')}}"></script>
-<script src="{{asset('assets/js/isotope.pkg.min.js')}}"></script>
-<script src="{{asset('assets/js/jquery.slick.min.js')}}"></script>
-<script src="{{asset('assets/js/mailchimp.min.js')}}"></script>
-<script src="{{asset('assets/js/counter.min.js')}}"></script>
-<script src="{{asset('assets/js/lightgallery.min.js')}}"></script>
-<script src="{{asset('assets/js/ripples.min.js')}}"></script>
-<script src="{{asset('assets/js/wow.min.js')}}"></script>
-<script src="{{asset('assets/js/jQueryUi.js')}}"></script>
-<script src="{{asset('assets/js/textRotate.min.js')}}"></script>
-<script src="{{asset('assets/js/select2.min.js')}}"></script>
-<script src="{{asset('assets/js/main.js')}}"></script>
+<script src="{{asset('assets/js/vendor/modernizr-3.5.0.min.js')}}?v={{$assets}}"></script>
+<script src="{{asset('assets/js/vendor/jquery-1.12.4.min.js')}}?v={{$assets}}"></script>
+<script src="{{asset('assets/js/isotope.pkg.min.js')}}?v={{$assets}}"></script>
+<script src="{{asset('assets/js/jquery.slick.min.js')}}?v={{$assets}}"></script>
+<script src="{{asset('assets/js/mailchimp.min.js')}}?v={{$assets}}"></script>
+<script src="{{asset('assets/js/counter.min.js')}}?v={{$assets}}"></script>
+<script src="{{asset('assets/js/lightgallery.min.js')}}?v={{$assets}}"></script>
+<script src="{{asset('assets/js/ripples.min.js')}}?v={{$assets}}"></script>
+<script src="{{asset('assets/js/wow.min.js')}}?v={{$assets}}"></script>
+<script src="{{asset('assets/js/jQueryUi.js')}}?v={{$assets}}"></script>
+<script src="{{asset('assets/js/textRotate.min.js')}}?v={{$assets}}"></script>
+<script src="{{asset('assets/js/select2.min.js')}}?v={{$assets}}"></script>
+<script src="{{asset('assets/js/main.js')}}?v={{$assets}}"></script>
 @yield('script')
 </body>
 </html>
