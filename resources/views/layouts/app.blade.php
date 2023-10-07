83af4b17e88090ec757fa48b5db8ebe43381ff38
Hazem <hazemgithub300@gmail.com>
2023-10-07 18:46:45 +0300

    u

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 @php
-    $assets = 1;
+    $assets = 2;
 @endphp
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{LaravelLocalization::setLocale() == 'en' ? 'ltr' : 'rtl'}}">
