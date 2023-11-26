0d69db23f4ba73d98f88ac4191fa2db0f28fac8b
Mohamed Yasser <100474135+Hazem7575@users.noreply.github.com>
2023-11-26 13:45:07 +0200

    u

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 @php
-    $assets = 14;
+    $assets = 15;
 @endphp
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{LaravelLocalization::setLocale() == 'en' ? 'ltr' : 'rtl'}}">
